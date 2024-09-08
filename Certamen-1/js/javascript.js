fbe42e7f3b75f897070e110a5a102681b9183d2c
Yama <KuChannel@hotmail.com>
2024-09-08 20:32:12 -0300

    Se termina casi toda la parte de formulario, queda agregar detalles y terminar validacion de los inputs

--- Certamen-1/js/javascript.js
+++ Certamen-1/js/javascript.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
     $("#products").hide();
     $("#form").hide();
+    $("#plastico").hide();
+    $("#papel").hide();
+    $("#vidrio").hide();
+    $("#metales").hide();
+    $("#electronicos").hide();
+    $("#subtipo").hide();
+
     
     $("#select-h").click(function () { 
         $("#homepage").show();
@@ -21,6 +28,64 @@ $(document).ready(function(){
         $("#form").show();
         
     });
+
+
+
+
+    $("#flexRadioDefault1").click(function(){
+        $("#subtipo").show();
+        $("#plastico").show();
+        $("#papel").hide();
+        $("#vidrio").hide();
+        $("#metales").hide();
+        $("#electronicos").hide();
+    })
+    $("#flexRadioDefault2").click(function(){
+        $("#subtipo").show();
+        $("#plastico").hide();
+        $("#papel").show();
+        $("#vidrio").hide();
+        $("#metales").hide();
+        $("#electronicos").hide();
+    })
+    $("#flexRadioDefault3").click(function(){
+        $("#subtipo").show();
+        $("#plastico").hide();
+        $("#papel").hide();
+        $("#vidrio").show();
+        $("#metales").hide();
+        $("#electronicos").hide();
+    })
+    $("#flexRadioDefault4").click(function(){
+        $("#subtipo").show();
+        $("#plastico").hide();
+        $("#papel").hide();
+        $("#vidrio").hide();
+        $("#metales").show();
+        $("#electronicos").hide();
+    })
+    $("#flexRadioDefault5").click(function(){
+        $("#subtipo").show();
+        $("#plastico").hide();
+        $("#papel").hide();
+        $("#vidrio").hide();
+        $("#metales").hide();
+        $("#electronicos").show();
+    })
+
+    $("#btn-enviar").click(function(){
+        //Revisar los select
+
+        //Revisar los input
+        console.log($("#input-nombre").val())
+        console.log($("#input-correo").val())
+        console.log($("#input-direccion").val())
+        console.log($("#floatingTextarea").val())
+
+        if($("#floatingTextarea").val()==null){
+            alert("npoo")
+        }
+    })
 
 });
 
