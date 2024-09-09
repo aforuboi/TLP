c51c3b479dcb1e436bbfe279bf3d725e8a227e02
Yama <KuChannel@hotmail.com>
2024-09-09 01:30:10 -0300

    se usa linter para mayor estetica

--- Certamen-1/js/javascript.js
+++ Certamen-1/js/javascript.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("#products").hide();
     $("#form").hide();
     $("#plastico").hide();
@@ -8,31 +8,31 @@ $(document).ready(function(){
     $("#electronicos").hide();
     $("#subtipo").hide();
 
-    
-    $("#select-h").click(function () { 
+
+    $("#select-h").click(function () {
         $("#homepage").show();
         $("#products").hide();
-        $("#form").hide(); 
+        $("#form").hide();
     });
 
-    $("#select-p").click(function () { 
+    $("#select-p").click(function () {
         $("#homepage").hide();
         $("#products").show();
         $("#form").hide();
-        
+
     });
 
-    $("#select-f").click(function () { 
+    $("#select-f").click(function () {
         $("#homepage").hide();
         $("#products").hide();
         $("#form").show();
-        
+
     });
 
 
 
 
-    $("#flexRadioDefault1").click(function(){
+    $("#flexRadioDefault1").click(function () {
         $("#subtipo").show();
         $("#plastico").show();
         $("#papel").hide();
@@ -40,7 +40,7 @@ $(document).ready(function(){
         $("#metales").hide();
         $("#electronicos").hide();
     })
-    $("#flexRadioDefault2").click(function(){
+    $("#flexRadioDefault2").click(function () {
         $("#subtipo").show();
         $("#plastico").hide();
         $("#papel").show();
@@ -48,7 +48,7 @@ $(document).ready(function(){
         $("#metales").hide();
         $("#electronicos").hide();
     })
-    $("#flexRadioDefault3").click(function(){
+    $("#flexRadioDefault3").click(function () {
         $("#subtipo").show();
         $("#plastico").hide();
         $("#papel").hide();
@@ -56,7 +56,7 @@ $(document).ready(function(){
         $("#metales").hide();
         $("#electronicos").hide();
     })
-    $("#flexRadioDefault4").click(function(){
+    $("#flexRadioDefault4").click(function () {
         $("#subtipo").show();
         $("#plastico").hide();
         $("#papel").hide();
@@ -64,7 +64,7 @@ $(document).ready(function(){
         $("#metales").show();
         $("#electronicos").hide();
     })
-    $("#flexRadioDefault5").click(function(){
+    $("#flexRadioDefault5").click(function () {
         $("#subtipo").show();
         $("#plastico").hide();
         $("#papel").hide();
@@ -73,19 +73,55 @@ $(document).ready(function(){
         $("#electronicos").show();
     })
 
-    $("#btn-enviar").click(function(){
+    $("#btn-enviar").click(function () {
         //Revisar los select
+        console.log("oal")
+
+        console.log($("#flexRadioDefault1:checked").val())
+        if ($("#flexRadioDefault1:checked").val() != undefined) {
+            if ($("#flexRadioDefault2:checked").val() != undefined) {
+                if ($("#flexRadioDefault3:checked").val() != undefined) {
+                    if ($("#flexRadioDefault4:checked").val() != undefined) {
+                        if ($("#flexRadioDefault5:checked").val() != undefined) {
+                        }else{
+                            alert("Electronicos no puede estar vacío")
+                        }
+                    }else{
+                        alert("Metales no puede estar vacío")
+                    }
+                }else{
+                    alert("Vidrio no puede estar vacío")
+                }
+            }else{
+                alert("Papel no puede estar vacío")
+            }
+        }else{
+            alert("Plastico no puede estar vacío")
+        }
 
         //Revisar los input
-        console.log($("#input-nombre").val())
-        console.log($("#input-correo").val())
-        console.log($("#input-direccion").val())
-        console.log($("#floatingTextarea").val())
-
-        if($("#floatingTextarea").val()==null){
-            alert("npoo")
+        if ($("#input-nombre").val().length > 0) {
+            if ($("#input-correo").val().length > 0) {
+                if ($("#input-direccion").val().length > 0) {
+                    if ($("#floatingTextarea").val().length > 0) {
+                        alert("Se guardó yupi!")
+                    }else{
+                        alert("Comentarios no puede estar vacío")
+                    }
+                }else{
+                    alert("Direccion no puede estar vacío")
+                }
+            }else{
+                alert("Correo no puede estar vacío")
+            }
+        }else{
+            alert("Nombre no puede estar vacío")
         }
     })
 
 });
 
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+  }
